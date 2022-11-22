a71be0175fbe7cbe45f1e48e432bff2a4563be93
boopathypradeep <boopathypradeep.rajasekaran@bounteous.com>
2022-11-22 11:42:24 +0530

    hello

--- src/test/java/com/practise/Practise/Browser1.java
+++ src/test/java/com/practise/Practise/Browser1.java
@@ -3,24 +3,7 @@ package com.practise.Practise;
 
 public class Browser1{
 public static void main(String[] args) {
-	String text="hello";
-	System.out.println(text);
-	String text="https://www.kkr.com/#";
-	String text="hello";
-	System.out.println(text);
-	String text="https://www.kkr.com/#";
-	String text="hello";
-	System.out.println(text);
-	String text="https://www.kkr.com/#";
-	String text="hello";
-	System.out.println(text);
-	String text="https://www.kkr.com/#";
-	String text="hello";
-	System.out.println(text);
-	String text="https://www.kkr.com/#";
-	String text="hello";
-	System.out.println(text);
-	String text="https://www.kkr.com/#";
+	
 }
 	
 
