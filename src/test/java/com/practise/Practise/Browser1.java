03f95f63fc61226e000f7a3b9016d5533d6ce44a
boopathypradeep <boopathypradeep.rajasekaran@bounteous.com>
2022-11-22 11:44:50 +0530

    hai

--- src/test/java/com/practise/Practise/Browser1.java
+++ src/test/java/com/practise/Practise/Browser1.java
@@ -1,9 +1,31 @@
 package com.practise.Practise;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Browser1{
 public static void main(String[] args) {
-	
+	  Calendar now = Calendar.getInstance();
+      SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+      String dateNow = formatter.format(now.getTime());
+      System.out.println("date Now:"+dateNow);
+       String fetch_date = dateNow.replaceAll("/", "_");
+       Calendar now = Calendar.getInstance();
+       SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+       String dateNow = formatter.format(now.getTime());
+       System.out.println("date Now:"+dateNow);
+        String fetch_date = dateNow.replaceAll("/", "_");
+        Calendar now = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        String dateNow = formatter.format(now.getTime());
+        System.out.println("date Now:"+dateNow);
+         String fetch_date = dateNow.replaceAll("/", "_");
+         Calendar now = Calendar.getInstance();
+         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+         String dateNow = formatter.format(now.getTime());
+         System.out.println("date Now:"+dateNow);
+          String fetch_date = dateNow.replaceAll("/", "_");
+             	
 }
 	
 
